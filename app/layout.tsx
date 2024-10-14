93247f14fea3a644870593e6208ab0ac2a24d3c0
melvan27 <119530410+melvan27@users.noreply.github.com>
2024-10-14 09:40:35 -0500

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Proper Shopper" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Providers>
